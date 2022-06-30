37270a5dbab950a53dc21d90b465f044b4ac3a10
KaichaoLiang <liangkc20@mails.tsinghua.edu.cn>
2022-06-30 16:46:52 +0800

    lkc0630

--- mmdet/models/roi_heads/__init__.py
+++ mmdet/models/roi_heads/__init__.py
@@ -26,6 +26,7 @@ from .adamixer_decoder import AdaMixerDecoder
 from .iteratemixer_decoder import IterateMixerDecoder
 from .iteratemixer_decoder_adapt import IterateMixerDecoderAdapt
 from .iteratemixer_decoder_withfcos import IterateMixerDecoderFcos
+from .iteratemixer_decoder_selfconv import IterateMixerDecoderSelfConv
 
 __all__ = [
     'BaseRoIHead', 'CascadeRoIHead', 'DoubleHeadRoIHead', 'MaskScoringRoIHead',
@@ -38,5 +39,5 @@ __all__ = [
     'CoarseMaskHead', 'DynamicRoIHead', 'SparseRoIHead', 'TridentRoIHead',
     'SCNetRoIHead', 'SCNetMaskHead', 'SCNetSemanticHead', 'SCNetBBoxHead',
     'FeatureRelayHead', 'GlobalContextHead', 'AdaMixerDecoder','IterateMixerDecoder'
-    ,'IterateMixerDecoder','IterateMixerDecoderFcos'
+    ,'IterateMixerDecoder','IterateMixerDecoderFcos','IterateMixerDecoderSelfConv'
 ]
