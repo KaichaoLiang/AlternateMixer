2b18329e08841adaa0f61c43d2483ff09092d75d
KaichaoLiang <liangkc20@mails.tsinghua.edu.cn>
2022-07-11 20:58:20 +0800

    lkc0711

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -7,6 +7,7 @@ from .double_bbox_head import DoubleConvFCBBoxHead
 from .sabl_head import SABLHead
 from .scnet_bbox_head import SCNetBBoxHead
 from .adamixer_decoder_stage import AdaMixerDecoderStage
+from .alternatemixer_decoder_stage import AlternateMixerDecoderStage
 from .adaptive_mixing_operator import AdaptiveMixing
 from .alternate_mixing_operator import AlternateMixing
 
@@ -14,4 +15,5 @@ __all__ = [
     'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
     'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'SABLHead', 'DIIHead',
     'SCNetBBoxHead', 'AdaMixerDecoderStage','AdaptiveMixing','AlternateMixing'
+    ,'AlternateMixerDecoderStage'
 ]
