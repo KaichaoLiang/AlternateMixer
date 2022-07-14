7663286ef978cd63f87fa0e5def87b36508e698e
KaichaoLiang <liangkc20@mails.tsinghua.edu.cn>
2022-07-14 15:52:13 +0800

    queryembedding

--- mmdet/models/roi_heads/__init__.py
+++ mmdet/models/roi_heads/__init__.py
@@ -30,7 +30,7 @@ from .iteratemixer_decoder_withfcos import IterateMixerDecoderFcos
 from .iteratemixer_decoder_selfconv import IterateMixerDecoderSelfConv
 from .iteratemixer_decoder_activate import IterateMixerDecoderActivate
 from .iteratemixer_decoder_dualconv import IterateMixerDecoderDualConv
-from .iteratemixer_decoder_adapt_largeconv import iteratemixer_decoder_adapt_largeconv
+from .iteratemixer_decoder_adapt_largeconv import IterateMixerDecoderAdaptLargeConv
 from .alternatemixer_decoder import AlternateMixerDecoder
 __all__ = [
     'BaseRoIHead', 'CascadeRoIHead', 'DoubleHeadRoIHead', 'MaskScoringRoIHead',
@@ -45,5 +45,5 @@ __all__ = [
     'FeatureRelayHead', 'GlobalContextHead', 'AdaMixerDecoder','IterateMixerDecoder'
     ,'IterateMixerDecoder','IterateMixerDecoderFcos','IterateMixerDecoderSelfConv',
     'IterateMixerDecoderActivate', 'IterateMixerDecoderFFN', 'IterateMixerDecoderDualConv',
-    'iteratemixer_decoder_adapt_largeconv','AlternateMixerDecoder'
+    'IterateMixerDecoderAdaptLargeConv','AlternateMixerDecoder'
 ]
