0ff23f09c16dc4457ba2919f5a55c034f8309df0
KaichaoLiang <liangkc20@mails.tsinghua.edu.cn>
2022-07-16 14:45:40 +0800

    lkc_maxkattention

--- mmdet/models/dense_heads/fcos_query_generator_attQuery.py
+++ mmdet/models/dense_heads/fcos_query_generator_attQuery.py
@@ -86,7 +86,7 @@ class FcosQueryGeneratorAttQuery(AnchorFreeHead):
         self.conv_centerness = nn.Conv2d(self.feat_channels, 1, 3, padding=1)
         self.scales = nn.ModuleList([Scale(1.0) for _ in self.strides])
         self.projector = nn.Linear(self.feat_channels, self.feat_channels)
-        self.attention = MultiheadAttention(self.feat_channels, 8, 0.0)
+        self.attention = MultiheadAttention(self.feat_channels, 8, 0.0, batch_first=True)
         self.attention_norm = build_norm_layer(dict(type='LN'), self.feat_channels)[1]
         self.init_content_features = nn.Embedding(self.num_query, self.feat_channels)
 
