b9ee738a443d4f902b2035f35a5a69bedbd722cc
KaichaoLiang <liangkc20@mails.tsinghua.edu.cn>
2022-07-28 20:21:45 +0800

    lkc

--- mmdet/models/roi_heads/bbox_heads/adamixer_decoder_stage_gcndense.py
+++ mmdet/models/roi_heads/bbox_heads/adamixer_decoder_stage_gcndense.py
@@ -37,7 +37,7 @@ def EuclideanDistances(a,b):
     #shape [batch, La, Lb]
     print('shape a', a.shape)
     print('shape b', bt.shape)
-    return torch.sqrt(sum_sq_a+sum_sq_b-2*torch.matmul(a,bt))
+    return torch.sqrt(sum_sq_a+sum_sq_b-2*torch.matmul(a.view(Ba, La, 1,Fa),bt))
 
 
 def dprint(*args, **kwargs):
