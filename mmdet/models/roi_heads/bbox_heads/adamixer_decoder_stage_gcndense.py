c39f2793b890c74b8ccd1f09c0a375adc3e0de50
KaichaoLiang <liangkc20@mails.tsinghua.edu.cn>
2022-07-28 21:21:13 +0800

    lkc

--- mmdet/models/roi_heads/bbox_heads/adamixer_decoder_stage_gcndense.py
+++ mmdet/models/roi_heads/bbox_heads/adamixer_decoder_stage_gcndense.py
@@ -166,7 +166,8 @@ class CrossGCNDense(nn.Module):
             
             adjacant_weight_topk_extend = adjacant_weight_topk.view(B, N*G, self.topk, 1).repeat(1, 1, 1, self.feat_dim)
             adjacant_index_topk_extend = adjacant_index_topk.view(B, N*G, self.topk, 1).repeat(1, 1, 1, self.feat_dim)
-            query_layer_update = torch.gather(sample_points.view(B,N*G*P,f), dim=-2, index=adjacant_index_topk_extend.view(B, N*G*self.topk,-1))*adjacant_weight_topk_extend.view(B, N*G*self.topk,-1)
+            sample_points_aug = sample_points.view(B,1,N*G*P,f).repeat(1,N*G,1,1)
+            query_layer_update = torch.gather(sample_points_aug, dim=-2, index=adjacant_index_topk_extend)*adjacant_weight_topk_extend
             query_layer_update = query_layer_update.view(B,N*G,self.topk,f)
             query_layer_update = torch.sum(query_layer_update,dim=-2).view(B, N*G, self.feat_dim)
             print('query update shape', query_layer_update.shape)
